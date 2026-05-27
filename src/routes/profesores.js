7ac4223bb14831feba54eef91fa66a521fb27e7b
Monse1011 <monseanayaalva@gmail.com>
2026-05-27 12:35:38 -0600

    AWS proyect

--- src/routes/profesores.js
+++ src/routes/profesores.js
@@ -5,12 +5,14 @@ const router = express.Router();
 
 function profesorValido(body) {
     return (
+        body.id &&
         body.nombres &&
         body.apellidos &&
         body.numeroEmpleado > 0 &&
         body.horasClase > 0
     );
 }
+
 router.post('/profesores', async (req, res) => {
     try {
 
